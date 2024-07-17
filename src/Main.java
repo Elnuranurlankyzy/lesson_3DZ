7530f67728d4b561132757f2c1b7e32c0101e5e3
Elnuranurlankyzy <elnura.nurlann@gmail.com>
2024-07-17 16:54:39 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
- import java.util.Arrays;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        int[][] numbers = new int[8][7];
-        numbers[2][4] = 7;
-        System.out.println(Arrays.deepToString(numbers));
-
-    }
-
-    public static void main() {
-        main(null);
-        int[] numbers = {8, -2, -4, 2, 3, 6, -7};
-        for (int k = 0; k < numbers.length; k++) {
-            if (numbers[k] < 2)
-            System.out.println(numbers[k]);
+        boolean flag = false;
+        int amountOfNumber = 0;
+        int sum = 0;
+        double[] numbers = {8, 2, 4, 2, -3, 6, -7, 84, 99,26,17,55,90,22};
+        System.out.println(Arrays.toString(numbers));
+        for (double num : numbers) {
+            if (num > 0) {
+                flag = true;
+            }
+            if (num > 0 && flag) {
+                amountOfNumber++;
+                sum += num;
+            }
         }
+        System.out.println(sum + "/" + amountOfNumber);
     }
 }
